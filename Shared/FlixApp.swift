72d4353ec0686a5d5f2c9cc47343d0e0af145dfd
Conrad Taylor <conradwt@gmail.com>
2021-05-19 18:13:39 -0700

    Initial Commit

---
+++ Shared/FlixApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlixApp.swift
+//  Shared
+//
+//  Created by Conrad Taylor on 5/19/21.
+//
+
+import SwiftUI
+
+@main
+struct FlixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
